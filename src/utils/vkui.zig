ca5ccb71e190b5604ee6df5f3086ac5e11335633
0x0de <p_0de-music@outlook.com>
2026-05-02 11:17:22 -0400

    Fix elements re-enabling after being added to others.

--- src/utils/vkui.zig
+++ src/utils/vkui.zig
@@ -291,7 +291,7 @@ pub const Element = struct
         var e = try self.allocator.create(Element);
         e.* = try .init(self.allocator, element.draw_mode, element.placement, element.coordinates);
 
-        e.enabled = self.enabled;
+        e.enabled = element.enabled;
         e.parent = self;
         e.space = element.space;
         e.freeze = element.freeze;
