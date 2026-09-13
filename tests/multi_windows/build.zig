73c9ee8f7904e754953159d9b58a4aeaef2faf2b
0x0de <p_0de-music@outlook.com>
2026-09-13 13:53:31 -0400

    Forgot to add multi_windows test to repo.

---
+++ tests/multi_windows/build.zig
@@ -0,0 +1,94 @@
+const std = @import("std");
+const builtin = @import("builtin");
+
+/// Adding other dependencies for ashbloom.
+fn add_library_dependencies(b: *std.Build, install_step: *std.Build.Step, ashbloom: *std.Build.Dependency) void
+{
+    const install_glfw = b.addInstallFile(ashbloom.path("deps/glfw.dll"), "bin/glfw.dll");
+    const install_freetype = b.addInstallFile(ashbloom.path("deps/libfreetype.dll"), "bin/libfreetype.dll");
+
+    const install_shader_ui_basic_vert = b.addInstallFile(ashbloom.path("src/utils/ui_themes/ui_basic_vert.spv"), "bin/shaders/ui_basic_vert.spv");
+    const install_shader_ui_basic_frag = b.addInstallFile(ashbloom.path("src/utils/ui_themes/ui_basic_frag.spv"), "bin/shaders/ui_basic_frag.spv");
+
+    install_step.dependOn(&install_glfw.step);
+    install_step.dependOn(&install_freetype.step);
+
+    install_step.dependOn(&install_shader_ui_basic_vert.step);
+    install_step.dependOn(&install_shader_ui_basic_frag.step);
+}
+
+/// Adding the Ashbloom library to the compile object.
+fn add_libraries(b: *std.Build, cmp: *std.Build.Step.Compile, ashbloom: *std.Build.Dependency, target: std.Build.ResolvedTarget, optimize: std.builtin.OptimizeMode) void
+{
+    cmp.root_module.addLibraryPath(.{ .cwd_relative = "zig-out/bin" });
+    
+    // Searching for the Vulkan drivers.
+    // On Windows, they're located in System32.
+    if(builtin.target.os.tag == .windows)
+    {
+        const sys32_path: std.Build.LazyPath = .{
+            .cwd_relative = "C:/Windows/System32"
+        };
+        
+        cmp.root_module.addLibraryPath(sys32_path);
+
+        const win32 = b.addModule("win32", .{
+            .root_source_file = b.path("../../lib/win32.zig"),
+            .target = target,
+            .optimize = optimize,
+            .link_libc = true
+        });
+
+        ashbloom.module("ashbloom").addImport("win32", win32);
+    }
+    else if(builtin.target.os.tag == .linux)
+    {
+        // TODO.
+    }
+
+    cmp.root_module.linkSystemLibrary("glfw", .{});
+    cmp.root_module.linkSystemLibrary("libfreetype", .{});
+    cmp.root_module.linkSystemLibrary("vulkan-1", .{});
+}
+
+pub fn build(b: *std.Build) void
+{
+    const std_target = b.standardTargetOptions(.{});
+    const std_optimize = b.standardOptimizeOption(.{});
+
+    const exe = b.addExecutable(.{
+        .name = "layouts",
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/main.zig"),
+            .target = std_target,
+            .optimize = std_optimize,
+            .link_libc = true
+	})
+    });
+    
+    const ashbloom = b.dependency("ashbloom", .{
+        .target = std_target,
+        .optimize = std_optimize,
+    });
+
+    exe.root_module.addImport("ashbloom", ashbloom.module("ashbloom"));
+    
+    // Building test .exe.
+
+    const install_exe = b.addInstallArtifact(exe, .{});
+    
+    add_library_dependencies(b, &install_exe.step, ashbloom);
+    
+    add_libraries(b, exe, ashbloom, std_target, std_optimize);
+
+    b.getInstallStep().dependOn(&install_exe.step);
+
+    // Run step.
+    
+    const run_step = b.step("run", "Run the application.");
+    const run_exe = b.addRunArtifact(exe);
+    run_exe.setCwd(b.path("./zig-out/bin"));
+
+    run_exe.step.dependOn(&install_exe.step);
+    run_step.dependOn(&run_exe.step);
+}
---
+++ tests/multi_windows/src/main.zig
@@ -0,0 +1,389 @@
+const std = @import("std");
+const ash = @import("ashbloom");
+
+const glfw = ash.glfw;
+const vk = ash.vk;
+
+const ui = ash.ui.core;
+
+var debug_required_validation_layers: [1][*:0]const u8 = .{
+    "VK_LAYER_KHRONOS_validation"
+};
+
+var debug_required_instance_extensions: [1][*:0]const u8 = .{
+    vk.extensions.ext_debug_utils.name
+};
+
+var required_device_extensions: [1][*:0]const u8 = .{
+    vk.extensions.khr_swapchain.name
+};
+
+const Window = ash.rendering.Window;
+
+var allocator: std.mem.Allocator = undefined;
+
+var window_a: Window = undefined;
+var window_b: Window = undefined;
+
+var vk_context: ash.rendering.VkContext = undefined;
+var vk_interface: ash.rendering.VkInterface = undefined;
+var vk_allocator: ash.utils.VulkanAllocator = undefined;
+
+var vk_command_pool: vk.CommandPool = undefined;
+
+const AppQueues = enum(u8)
+{
+    Graphics = 0,
+    Presentation,
+};
+
+var vk_queues: std.EnumArray(AppQueues, vk.Queue) = .initUndefined();
+
+/// Initializes the Vulkan context.
+fn init_vk_interfaces() !void
+{
+    const vk_context_options: ash.rendering.VkContext.InitOptions = .{
+        .instance_extensions = @ptrCast(&debug_required_instance_extensions),
+        .instance_layers = @ptrCast(&debug_required_validation_layers),
+    };
+
+    const vk_interface_options: ash.rendering.VkInterface.InitOptions = .{
+        .device_layers = @ptrCast(&debug_required_validation_layers),
+        .required_device_extensions = @ptrCast(&required_device_extensions),
+        .required_device_features = .{
+            .logic_op = .true
+        }
+
+    };
+
+    vk_context = try .init(&allocator, vk_context_options);
+
+    vk_interface = try .init(&vk_context, &window_a, vk_interface_options);
+    try vk_interface.add_window(&window_b);
+
+    const queue_families = vk_interface.physical_device_queue_families.?;
+
+    vk_queues.set(.Graphics, vk_interface.get_queue(@truncate(queue_families.graphics_family_index.?), 0));
+    vk_queues.set(.Presentation, vk_interface.get_queue(@truncate(queue_families.present_family_index.?), 0));
+
+    ash.print_stdout("({d}, {d}), ({d}, {d})\n", .{queue_families.graphics_family_index.?, queue_families.present_family_index.?, queue_families.graphics_family_index.?, queue_families.present_family_index.?});
+
+    vk_command_pool = try ash.rendering.commands.create_command_pool(&vk_interface, @truncate(queue_families.graphics_family_index.?));
+
+    vk_allocator = try .init(&vk_interface, &allocator, .{
+        .transfer_command_pool = &vk_command_pool,
+        .transfer_queue = vk_queues.getPtr(.Graphics),
+        .page_size = 128 << 20, // 128 MB.
+        .staging_size =  32 << 20 // 32 MB.
+    });
+}
+
+/// Deinitializes the Vulkan context.
+fn deinit_vk_interfaces() void
+{
+    vk_allocator.deinit();
+
+    vk_interface.device.destroyCommandPool(vk_command_pool, null);
+    vk_interface.deinit();
+
+    vk_context.deinit();
+}
+
+/// The application's first UI container.
+var app_ui_container_a: ui.Container = undefined;
+/// The application's second UI container.
+var app_ui_container_b: ui.Container = undefined;
+/// The application's chosen text font.
+var app_font: ash.ui.Font = undefined;
+
+fn init_ui_elements(container: *ui.Container) !void
+{
+    const background = try ash.ui.theme_basic.create_quad(&allocator, .{
+        .relative_pos = .{
+            .pos_x = 0,
+            .pos_y = 0,
+            .scl_x = 1,
+            .scl_y = 1
+        },
+        .absolute_offset = .get_default(),
+        .alignment = .{
+            .x = .Left,
+            .y = .Bottom
+        }
+    }, .{0.01, 0.01, 0.05, 1});
+
+    const linear_quad = try ash.ui.theme_basic.create_quad(&allocator, .{
+        .relative_pos = .{
+            .pos_x = 0,
+            .pos_y = 0,
+            .scl_x = 0.3,
+            .scl_y = 0.5,
+        },
+        .absolute_offset = .{
+            .pos_x = 30,
+            .pos_y = 30,
+            .scl_x = 0,
+            .scl_y = 0
+        },
+        .alignment = .{
+            .x = .Left,
+            .y = .Bottom
+        }
+    }, .{0.2, 0.2, 0.2, 1});
+
+    try ash.ui.layouts.set_layout_linear(linear_quad, .{
+        .primary_direction = .{
+            .margin = 5,
+            .spacing = 5,
+        },
+        .secondary_direction = .{
+            .margin = 5,
+            .spacing = 5,
+        },
+        .primary_axis = .Horizontal,
+        .alignment = .{
+            .x = .Left,
+            .y = .Center
+        }
+    });
+
+    const split_quad = try ash.ui.theme_basic.create_quad(&allocator, .{
+        .relative_pos = .{
+            .pos_x = 0.35,
+            .pos_y = 0,
+            .scl_x = 0.3,
+            .scl_y = 0.5
+        },
+        .absolute_offset = .{
+            .pos_x = 30,
+            .pos_y = 30,
+            .scl_x = 0,
+            .scl_y = 0
+        },
+        .alignment = .{
+            .x = .Left,
+            .y = .Bottom
+        }
+    }, .{0.2, 0.2, 0.2, 1});
+
+    try ash.ui.layouts.set_layout_split(split_quad, .{
+        .primary_axis = .Horizontal,
+        .primary_limit = 5
+    });
+
+    var random = std.Random.DefaultPrng.init(34891343);
+
+    for(0..8) |i|
+    {
+        const next_value_a = (random.next() % 150) + 50;
+        const next_value_b = (random.next() % 150) + 50;
+
+        const color: [4]f32 = switch(i % 4)
+        {
+            0 => .{0.2, 0.75, 0.2, 1},
+            1 => .{0.2, 0.25, 0.8, 1},
+            2 => .{0.8, 0.15, 0.25, 1},
+            else => .{0.6, 0.8, 0.15, 1},
+        };
+
+        const linear_child = try ash.ui.theme_basic.create_quad(&allocator, .{
+            .relative_pos = .{
+                .pos_x = 0,
+                .pos_y = 0,
+                .scl_x = 0,
+                .scl_y = 0
+            },
+            .absolute_offset = .{
+                .pos_x = 0,
+                .pos_y = 0,
+                .scl_x = @floatFromInt(next_value_a),
+                .scl_y = @floatFromInt(next_value_b)
+            },
+            .alignment = .{
+                .x = .Left,
+                .y = .Bottom
+            }
+        }, color);
+
+        try linear_quad.add_and_dispose(linear_child);
+    }
+
+    for(0..14) |i|
+    {
+        const color: [4]f32 = switch(i % 4)
+        {
+            0 => .{0.2, 0.75, 0.2, 1},
+            1 => .{0.2, 0.25, 0.8, 1},
+            2 => .{0.8, 0.15, 0.25, 1},
+            else => .{0.6, 0.8, 0.15, 1},
+        };
+
+        const split_child = try ash.ui.theme_basic.create_quad(&allocator, undefined, color);
+
+        try split_quad.add_and_dispose(split_child);
+    }
+
+    try background.add_and_dispose(linear_quad);
+    try background.add_and_dispose(split_quad);
+
+    try container.add_and_dispose(background);
+}
+
+pub fn main() !void
+{
+    var dba: std.heap.DebugAllocator(.{}) = .{};
+    defer {
+        const dba_result = dba.deinit();
+        if(dba_result == .leak)
+        {
+            std.debug.print("Program terminates with {d} memory leaks.\n", .{@intFromEnum(dba_result)});
+        }
+    }
+
+    allocator = dba.allocator();
+
+    try ash.init_graphics(&allocator);
+    defer ash.deinit_graphics();
+
+    glfw.windowHint(glfw.ClientAPI, glfw.NoAPI);
+
+    window_a = try .init(1280, 720, "Window 1");
+    window_b = try .init(1280, 720, "Window 2");
+
+    defer window_a.destroy();
+    defer window_b.destroy();
+
+    const system_fonts = try ash.utils.misc.enumerate_system_fonts(&allocator);
+
+    const names = [_][]const u8{"bahnschrift", "arial", "LiberationSans-Regular"};
+    const names_slc: []const []const u8 = &names;
+
+    const font_entry = try ash.utils.misc.search_font_entries(system_fonts, names_slc);
+
+    try init_vk_interfaces();
+    defer deinit_vk_interfaces();
+
+    var swapchain_a = try ash.rendering.Swapchain.init(&window_a, &vk_interface, &vk_allocator, vk_interface.surfaces.items[0], vk_command_pool, 1);
+    defer swapchain_a.deinit(true);
+
+    var swapchain_b = try ash.rendering.Swapchain.init(&window_b, &vk_interface, &vk_allocator, vk_interface.surfaces.items[1], vk_command_pool, 1);
+    defer swapchain_b.deinit(true);
+
+    try ui.init();
+    defer ui.deinit();
+
+    app_ui_container_a = try ui.Container.init(&vk_interface, &vk_allocator);
+    app_ui_container_b = try ui.Container.init(&vk_interface, &vk_allocator);
+
+    app_font = try ash.ui.Font.init(&vk_interface, &vk_allocator, font_entry.path, 36, &app_ui_container_a.texture_atlas);
+    
+    defer app_font.deinit();
+
+    for(system_fonts, 0..) |_, i|
+    {
+        system_fonts[i].deinit(&allocator);
+    }
+    allocator.free(system_fonts);
+
+    var container_resources_a = try ash.ui.theme_basic.init_render_instance(&vk_interface, &vk_allocator, swapchain_a, app_ui_container_a, null, vk_queues.get(.Graphics));
+    defer container_resources_a.deinit(vk_interface);
+    app_ui_container_a.set_render_instance(container_resources_a);
+
+    var container_resources_b = try ash.ui.theme_basic.init_render_instance(&vk_interface, &vk_allocator, swapchain_b, app_ui_container_b, null, vk_queues.get(.Graphics));
+    defer container_resources_b.deinit(vk_interface);
+    app_ui_container_b.set_render_instance(container_resources_b);
+
+    var framebuffers_ui_a = try swapchain_a.create_framebuffers(container_resources_a.render_pass, &.{});
+    defer framebuffers_ui_a.deinit(allocator);
+    defer swapchain_a.deinit_framebuffers(framebuffers_ui_a);
+
+    var framebuffers_ui_b = try swapchain_b.create_framebuffers(container_resources_b.render_pass, &.{});
+    defer framebuffers_ui_b.deinit(allocator);
+    defer swapchain_b.deinit_framebuffers(framebuffers_ui_b);
+
+    try init_ui_elements(&app_ui_container_a);
+    try init_ui_elements(&app_ui_container_b);
+
+    var cur_size = window_a.get_framebuffer_size();
+
+    try app_ui_container_a.set_bounds(0, 0, @floatFromInt(cur_size.width), @floatFromInt(cur_size.height));
+    try app_ui_container_a.build();
+    
+    try app_ui_container_b.set_bounds(0, 0, @floatFromInt(cur_size.width), @floatFromInt(cur_size.height));
+    try app_ui_container_b.build();
+
+    while(!window_a.should_close())
+    {
+        const prev_window_size: vk.Extent2D = .{
+            .width = cur_size.width,
+            .height = cur_size.height
+        };
+
+        glfw.pollEvents();
+
+        var acquire_result = try swapchain_a.acquire_next_image();
+        if(acquire_result == .NewSwapchain)
+        {
+            swapchain_a.deinit_framebuffers(framebuffers_ui_a);
+            framebuffers_ui_a.deinit(allocator);
+            framebuffers_ui_a = try swapchain_a.create_framebuffers(container_resources_a.render_pass, &.{});
+        }
+
+        cur_size = window_a.get_framebuffer_size();
+
+        if(cur_size.width != prev_window_size.width or cur_size.height != prev_window_size.height)
+        {
+            try app_ui_container_a.set_bounds(0, 0, @floatFromInt(cur_size.width), @floatFromInt(cur_size.height));
+        }
+
+        var container_input = window_a.get_ui_container_input();
+
+        try app_ui_container_a.update(container_input);
+
+        if(app_ui_container_a.signal_reset_manual_input)
+        {
+            ash.rendering.Window.reset_input_values();
+            app_ui_container_a.signal_reset_manual_input = false;
+        }
+
+        var command_buffer = try swapchain_a.get_next_command_buffer();
+        try app_ui_container_a.draw(command_buffer, &swapchain_a, framebuffers_ui_a.items[swapchain_a.current_image_index]);
+
+        try swapchain_a.present(vk_queues.get(.Presentation));
+        
+        acquire_result = try swapchain_b.acquire_next_image();
+        if(acquire_result == .NewSwapchain)
+        {
+            swapchain_b.deinit_framebuffers(framebuffers_ui_b);
+            framebuffers_ui_b.deinit(allocator);
+            framebuffers_ui_b = try swapchain_b.create_framebuffers(container_resources_b.render_pass, &.{});
+        }
+
+        cur_size = window_b.get_framebuffer_size();
+
+        if(cur_size.width != prev_window_size.width or cur_size.height != prev_window_size.height)
+        {
+            try app_ui_container_b.set_bounds(0, 0, @floatFromInt(cur_size.width), @floatFromInt(cur_size.height));
+        }
+
+        container_input = window_b.get_ui_container_input();
+
+        try app_ui_container_b.update(container_input);
+
+        if(app_ui_container_b.signal_reset_manual_input)
+        {
+            ash.rendering.Window.reset_input_values();
+            app_ui_container_b.signal_reset_manual_input = false;
+        }
+
+        command_buffer = try swapchain_b.get_next_command_buffer();
+        try app_ui_container_b.draw(command_buffer, &swapchain_b, framebuffers_ui_b.items[swapchain_b.current_image_index]);
+
+        try swapchain_b.present(vk_queues.get(.Presentation));
+    }
+
+    try vk_interface.device.deviceWaitIdle();
+
+    try app_ui_container_b.deinit();
+    try app_ui_container_a.deinit();
+}
