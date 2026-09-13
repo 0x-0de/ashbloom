bd18b26c5b12d223558db0cb232a623e267a5985
0x0de <p_0de-music@outlook.com>
2026-09-13 14:06:41 -0400

    Remove res folder

--- res/shaders/compile_shaders.bat
+++
@@ -1,4 +0,0 @@
-C:/VulkanSDK/1.4.321.1/Bin/glslc.exe main.vert -o main_vert.spv
-C:/VulkanSDK/1.4.321.1/Bin/glslc.exe main.frag -o main_frag.spv
-C:/VulkanSDK/1.4.321.1/Bin/glslc.exe ui.vert -o ui_vert.spv
-C:/VulkanSDK/1.4.321.1/Bin/glslc.exe ui.frag -o ui_frag.spv
